4f49039beb77afffc021537fe5607bd5129a33c0
mohamad <128635535+1myh@users.noreply.github.com>
2025-02-01 22:02:07 +0300

    github pages next js i guess

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
 	basePath: "/cv",
 	output: "export",
 	reactStrictMode: true,
+	i18n: {
+		locales: ["ar", "en"],
+		defaultLocale: "en",
+	}
 };
  
 export default withNextIntl(nextConfig);
